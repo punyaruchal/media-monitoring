823b992596e796e329d3f72b5b7003793fbc202b
Nation <shakyanation@gmail.com>
2021-05-06 10:04:05 +0545

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,35 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        heroBg: '#FEEBCF',
+        primary: '#007663',
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+        SerifDisplay: ['DM Serif Display']
+      }
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+      'xsm': {'max': '560px'},
+      
+      '2xsm': {'max': '480px'},
+    }
   },
   variants: {
     extend: { borderWidth: ['last'] },
